86573495fc5d00b63671b8505fc4e86b7af26fa4
maroua1902 <maroua.alioua05@gmail.com>
2026-01-29 17:00:25 +0100

    Clean project: ignore cache, add requirements and update settings

--- CinemaProject/settings.py
+++ CinemaProject/settings.py
@@ -128,4 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Films/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
